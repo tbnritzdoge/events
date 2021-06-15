56fb868b3a9381981ee34cf1f0af99b8bce99bac
tbnritzdoge <shadowphoenixcoc@gmail.com>
2021-06-15 19:15:18 -0400

    feat: add files

---
+++ README.md
@@ -0,0 +1,9 @@
+
+Under extremely heavy load with 100 constant listeners @tbnritzdoge/events outperforms any other emitter
+
+```
+drip x 519,385 ops/sec ±0.92% (94 runs sampled)
+native x 450,977 ops/sec ±1.62% (85 runs sampled)
+eventemitter3 x 427,221 ops/sec ±5.45% (90 runs sampled)
+@tbnritzdoge/events x 1,248,102 ops/sec ±0.25% (95 runs sampled)
+```
---
+++ src/index.js
@@ -0,0 +1,76 @@
+function indexOf(arr, element, fromIndex = 0) {
+    for (var i = fromIndex, L = arr.length; i !== L; ++i) if (arr[i] === element) return i;
+    return -1;
+}
+/**
+ * @class Emitter
+ */
+class Emitter {
+    #events = {};
+    
+    /**
+     * @param {string} event
+     * @param {any} fn
+     * @return {any} 
+     * @memberof Emitter
+     */
+    on(event, fn) {
+        const e = this.#events[event];
+        return e ? e[e.length] = fn : this.#events[event] = [fn];
+    }
+
+    /**
+     * @param {string} event
+     * @param {...string} args
+     * @return {undefined} 
+     * @memberof Emitter
+     */
+    emit(event, ...args) {
+        const e = this.#events[event];
+        if (!e) return;
+        var i = e.length;
+        while (i--) e[i](...args);
+    }
+
+    /**
+     * @param {string} event
+     * @param {any} fn
+     * @return {any} 
+     * @memberof Emitter
+     */
+    once(event, fn) {
+        const f = (...args) => {
+            this.splice(event, fn);
+            fn(...args);
+        }
+
+        return this.on(event, f);
+    }
+
+    /**
+     * @param {string} event
+     * @return {any}
+     * @memberof Emitter
+     */
+    off(event) {
+        return this.#events[event] = void 0;
+    }
+
+    /**
+     * @param {string} event
+     * @param {fn} the function which you provided in <Emitter>.on or <Emitter>.once
+     * @memberof Emitter
+     */
+    splice(event, fn) {
+        const fns = this.#events[event];
+        if (fns.length === 1) this.off(event);
+        else fns.splice(indexOf(fns, fn), 1);
+    }
+
+    addListener(n, t) { return this.on(n, t) }
+    removeListener(n, t) { return this.splice(n, t) }
+    setMaxListeners(..._) { }
+    getMaxListeners() { return Infinity }
+}
+
+module.exports = Emitter;
