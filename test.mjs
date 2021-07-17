254686496d6c6ee10e745e924032f98c4c357427
tbnritzdoge <shadowphoenixcoc@gmail.com>
2021-07-17 00:02:43 -0400

    refactor(tests): seperate tests better

--- test.mjs
+++ test.mjs
@@ -12,6 +12,8 @@ test('static', () => {
     const emitter = new Emitter();
     assert.ok(emitter);
 
+    assert.instance(emitter, Emitter);
+    assert.equal(emitter.eventNames().length, 0);
     assert.throws(emitter['#events']);
     assert.equal(emitter.getMaxListeners(), Infinity);
     assert.ok(emitter.setMaxListeners(2000));
@@ -50,33 +52,43 @@ test('shims', () => {
     assert.ok(emitter.off('random', noop));
 });
 
-test('emitter', () => {
+test('on', () => {
     const emitter = new Emitter();
-
-    assert.ok(emitter);
-    assert.instance(emitter, Emitter);
-
-    assert.equal(emitter.eventNames().length, 0);
 
     const on = emitter.on('test', noop);
     assert.instance(on, Emitter);
     assert.equal(emitter.listenerCount('test'), 1);
     assert.equal(emitter.listenerCount('random'), 0);
 
+    emitter.on('test', () => null);
+
+    const emitted = emitter.emit('test', 'testist');
+    assert.ok(emitted);
+
+    emitter.removeListener('test', noop);
+    assert.equal(emitter.listenerCount('test'), 1);
+
+    const emitted2 = emitter.emit('test', 'testist');
+    assert.ok(emitted2);
+
+    assert.ok(emitter.off('test'));
+    assert.equal(emitter.listenerCount('test'), 0);
+
+    const emitted3 = emitter.emit('test', 'testist');
+    assert.not.ok(emitted3);
+});
+
+test('once', () => {
+    const emitter = new Emitter();
+
     emitter.once('test', () => null);
-    assert.equal(emitter.listenerCount('test'), 2);
+    assert.equal(emitter.listenerCount('test'), 1);
 
     const emitted = emitter.emit('test', 'testist');
     assert.ok(emitted);
 
     const emitted2 = emitter.emit('test', 'testist');
-    assert.ok(emitted2);
-
-    emitter.removeListener('test', noop);
-    assert.equal(emitter.listenerCount('test'), 0);
-
-    const emitted3 = emitter.emit('test', 'testist');
-    assert.not.ok(emitted3);
+    assert.not.ok(emitted2);
 });
 
 test.run();
