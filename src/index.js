74faabae9b3abf869892b1de7c7d82f86137c41a
tbnritzdoge <shadowphoenixcoc@gmail.com>
2021-09-08 22:26:09 -0400

    feat: various performance improvements & cleanup
    * benchmarks for indexOf switch https://cdn.discordapp.com/attachments/799365819242381353/885348296636375090/unknown.png

--- src/index.js
+++ src/index.js
@@ -1,64 +1,59 @@
-/* c8 ignore start */
-function indexOf(arr, element, fromIndex = 0) {
-    for (var i = fromIndex, L = arr.length; i !== L; ++i) if (arr[i] === element) return i;
-    return -1;
-}
-/* c8 ignore stop */
+function nullObjProto() { var a = {}; Object.setPrototypeOf(a, null); return a; };
 class Emitter {
-    #events = {};
-    constructor() { Object.setPrototypeOf(this.#events, null); };
+    #events;
+    constructor() { this.#events = nullObjProto(); };
 
     on(event, fn) {
         const e = this.#events[event] ??= [];
         return e[e.length] = fn, this;
-    }
+    };
 
     emit(event, ...args) {
         const e = this.#events[event];
-        if (Array.isArray(e) === false) return false;
+        if (Array.isArray(e) == false) return false;
         var i = e.length;
         while (i--) e[i](...args);
         return true;
-    }
+    };
 
     once(event, fn) {
         const f = (...args) => {
             this.off(event, fn);
             fn(...args);
-        }
+        };
 
         return this.on(event, f);
-    }
+    };
 
     off(event, fn) {
         const fns = this.#events[event];
-        if (fns === undefined) return this;
-        if (fns.length === 1) this.#events[event] = void 0;
-        else fns.splice(indexOf(fns, fn), 1);
+        if (fns == undefined) return this;
+        if (fns.length == 1) this.#events[event] = void 0;
+        else fns.splice(fns.indexOf(fn), 1);
         return this;
-    }
+    };
 
     listenerCount(event) {
         const fns = this.#events[event];
-        if (fns === undefined) return 0;
+        if (fns == undefined) return 0;
         return fns.length;
-    }
+    };
 
     removeAllListeners(event) {
-        if (event === undefined) this.#events = Object.create(null);
+        if (event == undefined) this.#events = nullObjProto();
         else this.#events[event] = void 0;
         return this;
-    }
+    };
 
-    prependListener(event, fn) { return this.on(event, fn); }
-    prependOnceListener(event, fn) { return this.once(event, fn); }
-    listeners(event) { return this.rawListeners(event); }
-    rawListeners(event) { return this.#events[event] ?? []; }
-    eventNames() { return Object.keys(this.#events); }
-    removeListener(e, f) { return this.off(e, f); }
-    addListener(n, t) { return this.on(n, t); }
-    setMaxListeners(..._) { return this; }
-    getMaxListeners() { return Infinity }
-}
+    prependListener(event, fn) { return this.on(event, fn); };
+    prependOnceListener(event, fn) { return this.once(event, fn); };
+    listeners(event) { return this.rawListeners(event); };
+    rawListeners(event) { return this.#events[event] ?? []; };
+    eventNames() { return Object.keys(this.#events); };
+    removeListener(e, f) { return this.off(e, f); };
+    addListener(n, t) { return this.on(n, t); };
+    setMaxListeners(..._) { return this; };
+    getMaxListeners() { return Infinity; };
+};
 
 module.exports = Emitter;
